537a3d1575ad119eee4581a9fe0be84ce2eef44d
James Stanley <james@incoherency.co.uk>
2022-04-29 17:08:26 +0100

    Start on golang implementation of backend

---
+++ go/tool.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+type Tool interface {
+    HeightAtRadius(float64) float64
+}
+
+type BallEndMill struct {
+    radius float64
+}
+type FlatEndMill struct {
+    radius float64
+}
+
+func NewTool(tooltype string, diameter float64) (Tool, error) {
+    if tooltype == "flat" {
+        return FlatEndMill{radius: diameter/2}, nil
+    } else if tooltype == "ball" {
+        return BallEndMill{radius: diameter/2}, nil
+    } else {
+        return nil, fmt.Errorf("unrecognised tool type: %s", tooltype)
+    }
+}
+
+func (t BallEndMill) HeightAtRadius(r float64) float64 {
+    if r > t.radius {
+        return math.Inf(1)
+    }
+
+    return t.radius - math.Sqrt(t.radius*t.radius - r*r);
+}
+
+func (t FlatEndMill) HeightAtRadius(r float64) float64 {
+    if r > t.radius {
+        return math.Inf(1)
+    }
+
+    return 0
+}
---
+++ go/tool_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "math"
+    "testing"
+)
+
+func TestBall(t *testing.T) {
+    tool, err := NewTool("ball", 10)
+    if err != nil {
+        t.Errorf("can't create ball tool: %v", err)
+    }
+
+    checkHeightAtRadius(t, tool, 0, 0)
+    checkHeightAtRadius(t, tool, 5, 5)
+    checkHeightAtRadius(t, tool, 1, 0.1010205)
+    checkHeightAtRadius(t, tool, 3, 1)
+    checkHeightAtRadius(t, tool, 6, math.Inf(1))
+
+    tool, err = NewTool("flat", 10)
+    if err != nil {
+        t.Errorf("can't create flat tool: %v", err)
+    }
+
+    checkHeightAtRadius(t, tool, 0, 0)
+    checkHeightAtRadius(t, tool, 5, 0)
+    checkHeightAtRadius(t, tool, 1, 0)
+    checkHeightAtRadius(t, tool, 3, 0)
+    checkHeightAtRadius(t, tool, 6, math.Inf(1))
+}
+
+func checkHeightAtRadius(t *testing.T, tool Tool, r float64, wantheight float64) {
+    epsilon := 0.00001
+
+    h := tool.HeightAtRadius(r)
+
+    if math.Abs(h-wantheight) > epsilon {
+        t.Errorf("height at radius %v should be %v, got %v", r, wantheight, h)
+    }
+}
