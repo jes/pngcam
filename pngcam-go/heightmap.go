1ec996dc91f584e27aebbc9f0ca3ec3bc4a4ce93
James Stanley <james@incoherency.co.uk>
2023-08-14 07:03:10 +0100

    Add workpiece depth to cut depth sooner, to fix the trigonometry

--- pngcam-go/heightmap.go
+++ pngcam-go/heightmap.go
@@ -63,7 +63,7 @@ func (hm *HeightmapImage) CutDepth(x, y float64) float64 {
 	if opt.rotary {
 		for sy := -90.0; sy <= 90.0; sy += opt.y_MmPerPx { // we pretend the y range of 360 degrees is 360 "millimetres"
 			for sx := -tool.Radius(); sx <= tool.Radius(); sx += opt.x_MmPerPx {
-				workpieceZ := hm.GetDepth(x+sx, -y+sy) // -y because the heightmap y axis is inverted (?)
+				workpieceZ := opt.depth + hm.GetDepth(x+sx, -y+sy) // -y because the heightmap y axis is inverted (?)
 				realY := workpieceZ * math.Sin(sy*math.Pi/180.0)
 				realZ := workpieceZ * math.Cos(sy*math.Pi/180.0)
 
@@ -76,7 +76,7 @@ func (hm *HeightmapImage) CutDepth(x, y float64) float64 {
 
 				// TODO: what about if !opt.cutBelowBottom || !hm.IsBottom(x+sx, ...) ?
 
-				d := z + realZ + opt.depth
+				d := z + realZ
 				if d > maxDepth {
 					maxDepth = d
 				}
